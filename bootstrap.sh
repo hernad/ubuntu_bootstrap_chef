560e51a508a629693b8adb89f6738dbef150ed5d
Ernad Husremovic <hernad@bring.out.ba>
2012-11-08 12:50:08 +0100

    admin user

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,10 +8,18 @@
 # https://gist.github.com/3328844
 
 GITHUB_USER=hernad
-
+ADMIN_USER=admin
+HOME_DIR=/home/$ADMIN_USER
 # http://www.wolfe.id.au/2012/09/10/how-i-use-chef-solo-with-ubuntu-12.04/
 
 #bash < <( curl -L https://raw.github.com/gist/73344/vakonako )
+
+# http://serverfault.com/questions/58378/add-new-user-with-root-access-in-linux
+
+create_admin_user() {
+  useradd $ADMIN_USER -m -s /bin/bash
+  usermod -a -G adm
+}
 
 upgrade_system() {
   apt-get -y update
@@ -33,9 +41,12 @@ mkdir /var/chef && chown admin:admin /var/chef
 mkdir -p /home/admin/.ssh && chown admin:admin
 chown 0600 /home/admin/.ssh
 
-cat (CURL -L https://raw.github.com/hernad/ubuntu_bootstrap_chef/master/sezame_otvori_se.pub) >> /home/admin/autorized_keys
+cat (CURL -L https://raw.github.com/$GITHUB_USER/ubuntu_bootstrap_chef/master/sezame_otvori_se.pub) >> $HOME_DIR/.ssh/autorized_keys
 
 }
+
+echo "create admin user ..."
+create_admin_user
 
 echo "upgrading system ..."
 upgrade_system
