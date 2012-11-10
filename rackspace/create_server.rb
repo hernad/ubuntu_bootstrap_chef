992f13c3dd01e04aa9c5f1fd1f41a00237d26574
Ernad Husremovic <hernad@bring.out.ba>
2012-11-10 20:45:31 +0100

    ako server sa imenom OS_SERVER_NAME vec postoji nemoj raditi bootstrap

---
+++ rackspace/create_server.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+#
+cd = Dir.pwd
+
+require cd + "/rackspace_server"
+
+r = RackServer.new
+
+name = ENV['OS_SERVER_NAME']
+
+s = r.find_server_by_name(name)
+
+
+if s.nil?
+   r.bootstrap
+else
+   print "Server sa imenom #{s.name} vec postoji !"
+   print "Server: #{s.name} IP: #{s.public_ip_address}"
+end
+
+r.wait_until_active
+
+
