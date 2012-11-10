a6ad5e21857000ad87d3a127f2a074c3cf1f64e3
Ernad Husremovic <hernad@bring.out.ba>
2012-11-10 20:34:33 +0100

    cleanup

--- rackspace/run_rack.sh
+++ rackspace/run_rack.sh
@@ -17,12 +17,14 @@ ssh -o $SSH_OPT -i $PRIVATE_KEY $USER@$PUBLIC_IP $1
 
 #CMD=`ruby export_public_ip.rb`
 
-PUBLIC_IP=`./export_public_ip.rb | sed -e 's/export OS_PUBLIC_IP=//'`
+#PUBLIC_IP=`./export_public_ip.rb | sed -e 's/export OS_PUBLIC_IP=//'`
+PUBLIC_IP=`ruby public_ip.rb`
 
 echo "PUBLIC_IP=$PUBLIC_IP"
 
-echo ./manage_domains.py  -d $OS_DNS_DOMAIN -r $OS_SERVER_NAME -i `./public_ip.rb`
-./manage_domains.py  -d $OS_DNS_DOMAIN -r $OS_SERVER_NAME -i `./public_ip.rb`
+echo "vrsim update domena $OS_DNS_DOMAIN, zapis $OS_SERVER_NAME sa javnom adresom $PUBLIC_IP"
+echo ./manage_domains.py  -d $OS_DNS_DOMAIN -r $OS_SERVER_NAME -i $PUBLIC_IP
+./manage_domains.py  -d $OS_DNS_DOMAIN -r $OS_SERVER_NAME -i $PUBLIC_IP
 
 export $OS_ENVARS
 
@@ -34,7 +36,3 @@ BASH_F="boostrap_project.sh"
 CMD="apt-get install -y curl && ( echo -e 'export $OS_ENVARS\n' > $BASH_F && curl -L $BOOTSTRAP_URL >> $BASH_F && bash $BASH_F )"
 run_ssh "$CMD"
 
-
-#nova reboot [--hard] [--poll] <server>
-#echo rebooting server
-#nova reboot $SERVER_NAME
